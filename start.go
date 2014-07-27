dfc6dcbdafac9e0c97472d4342f221ccb496942c
Christoph Berger <github@christophberger.com>
2014-07-27 17:27:13 +0000

    New file: start.go. License personalized.

---
+++ start.go
@@ -0,0 +1,11 @@
+// Copyright 2014 Christoph Berger. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// See the file README.md about usage of the start package.
+
+package start
+
+import (
+    "github.com/ogier/pflag"
+)
