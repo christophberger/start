7811771fc5b41331a5d4277c491121483642c434
Christoph Berger <github@christophberger.com>
2014-08-20 18:23:31 +0200

    TestParse() implemented

--- start_test.go
+++ start_test.go
@@ -1,21 +1,39 @@
 package start
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
+	"os"
 	"testing"
+
+	flag "github.com/ogier/pflag"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestParse(t *testing.T) {
-	SkipConvey("", t, func() {
-		var x int = 1
-		Convey("", func() {
-			x++
+	// ContinueOnError is required when running goconvey as server; otherwise, unrecognized
+	// variables are passed to the test executable, causing an error:
+	// "unknown shorthand flag: 't' in -test.v=true"
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
 
-			Convey("", func() {
-				So(x, ShouldEqual, 2)
-			})
+	Convey("When setting up some flag variables", t, func() {
+		var stringFlag string
+		flag.StringVarP(&stringFlag, "astring", "a", "From Default", "A string flag")
+		intFlag := flag.IntP("anint", "i", 23, "An integer flag")
+		boolFlag := flag.BoolP("anewbool", "b", true, "A boolean flag")
+		UseConfigFile("test/test.toml")
+		os.Setenv("START_ASTRING", "From Environment Variable")
+		Parse()
+		Convey("Then Parse() should find the correct values from config file, env var, or default. (command line flags not testable with automated calls to go test)", func() {
+			So(stringFlag, ShouldEqual, "From Environment Variable")
+			So(*intFlag, ShouldEqual, 42)    // from config file
+			So(*boolFlag, ShouldEqual, true) // from default
+
+		})
+		Reset(func() {
+			os.Setenv("START_ASTRING", "")
 		})
 	})
+
 }
 
 func TestUp(t *testing.T) {
