0cb30a58d2c5dd87107bd410cfa98bdd92092d34
Christoph Berger <github@christophberger.com>
2014-09-15 23:02:05 +0200

    Added: Behavior if no config file name is specified. Changed: readTomlFile creates an empty TOML doc by parsing an empty string. Minor changes.

--- toml.go
+++ toml.go
@@ -27,19 +27,20 @@ type ConfigFile struct {
 // NewConfigFile creates a new ConfigFile struct filled with the contents
 // of the file identified by filename.
 // Parameter filename can be an empty string, a file name, or a fully qualified path.
-func NewConfigFile(filename string) *ConfigFile {
-	cfg := new(ConfigFile)
+func NewConfigFile(filename string) (*ConfigFile, error) {
+	cfg := &ConfigFile{}
 	err := cfg.findAndReadTomlFile(filename)
-	if err != nil {
-		return nil
-	}
-	return cfg
+	return cfg, err
 }
 
 // String returns the value of key "name" as a string.
 // Keys must be defined outside any section in the TOML file.
 func (c *ConfigFile) String(name string) string {
 	value, exists := c.doc.GetValue(name)
+	// Note: c.doc.GetString() does not work here as this
+	// returns "" for all non-string values.
+	// GetValue().String(), on the other hand, does work for
+	// all non-string values that implement the String() method.
 	if exists {
 		return value.String()
 	} else {
@@ -78,7 +79,14 @@ func (c *ConfigFile) findAndReadTomlFile(name string) error {
 	// so get the user's home dir instead
 	cfgPath = GetHomeDir()
 	if len(cfgPath) > 0 {
-		c.doc, err = c.readTomlFile(filepath.Join(cfgPath, name))
+		var path string
+		if len(name) == 0 {
+			// no name supplied; in $HOME use .<application>
+			path = filepath.Join(cfgPath, "."+AppName())
+		} else {
+			path = filepath.Join(cfgPath, name)
+		}
+		c.doc, err = c.readTomlFile(path)
 		if err == nil {
 			return nil
 		}
@@ -89,6 +97,9 @@ func (c *ConfigFile) findAndReadTomlFile(name string) error {
 	// so try the working dir instead
 	cfgPath, err = os.Getwd()
 	if err == nil {
+		if len(name) == 0 {
+			name = AppName() + ".toml"
+		}
 		c.doc, err = c.readTomlFile(filepath.Join(cfgPath, name))
 		return err
 	}
@@ -97,11 +108,11 @@ func (c *ConfigFile) findAndReadTomlFile(name string) error {
 
 func (c *ConfigFile) readTomlFile(path string) (toml.Document, error) {
 	var parser toml.Parser
-	var doc toml.Document
+	emptyDoc := parser.Parse("") // empty default TOML document required to fix a runtime panic
 	if _, err := os.Stat(path); err == nil {
 		return parser.ParseFile(path), nil
 	}
-	return doc, errors.New("File not found: " + path)
+	return emptyDoc, errors.New("File not found: " + path)
 }
 
 // GetHomeDir finds the user's home directory in an OS-independent way.
