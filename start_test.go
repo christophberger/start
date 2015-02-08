3ac80769d74bbfabb2f83b09f4b4564d2104b217
Christoph Berger <github@christophberger.com>
2015-02-08 21:14:33 +0100

    Tests adapted to the changes in start.go

--- start_test.go
+++ start_test.go
@@ -24,7 +24,7 @@ func TestParse(t *testing.T) {
 		intFlag := flag.IntP("anint", "i", 23, "An integer flag")
 		boolFlag := flag.BoolP("anewbool", "b", true, "A boolean flag")
 
-		UseConfigFile("test/test.toml")
+		SetConfigFile("test/test.toml")
 		os.Setenv("START_ASTRING", "From Environment Variable")
 		Parse()
 		Convey("Then Parse() should find the correct values from config file, env var, or default. (Restriction: passing the command line flags is not possible with automated calls to go test)", func() {
@@ -47,10 +47,10 @@ func TestUp(t *testing.T) {
 	var global int
 	var params []string
 
-	GlobalInit = func() error {
+	SetInitFunc(func() error {
 		SetDescription("Testing testcmd")
 		return nil
-	}
+	})
 
 	// ContinueOnError is required when running goconvey as server; otherwise, unrecognized
 	// flags that are passed to the test executable will cause an error:
@@ -80,13 +80,19 @@ func TestUp(t *testing.T) {
 		},
 	})
 
+	Up()
 	Convey("The test command should read all flags and parameters.", t, func() {
-		Up()
 		So(first, ShouldEqual, 10)
 		So(second, ShouldEqual, 20)
 		So(global, ShouldEqual, 3)
 		So(params[0], ShouldEqual, "arg1")
 		So(params[1], ShouldEqual, "arg2")
 		So(description, ShouldEqual, "Testing testcmd")
+	})
+	Convey("The test command should contain only its private flags.", t, func() {
+		cmd := Commands["testcmd"]
+		So(len(cmd.Flags), ShouldEqual, 2)
+		So(cmd.Flags[0], ShouldEqual, "first")
+		So(cmd.Flags[1], ShouldEqual, "second")
 	})
 }
